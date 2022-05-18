3869110bc035efe93e64edbb2ef24f7b31e6ffc4
ganeshhp <ganeshhp@gmail.com>
2022-05-18 09:56:33 +0530

    updates to project files

--- website/index.php
+++ website/index.php
@@ -7,7 +7,7 @@
 	    <h2>Candidate Information Form</h2>
 	    <ul>
 		<?php
-  		    $json = file_get_contents('http://product-service');
+  		    $json = file_get_contents('http://product-service:80');
   		    $obj = json_decode($json);
 		    $products = $obj->products;
 		    foreach ($products as $product) {
